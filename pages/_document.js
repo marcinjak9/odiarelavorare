1505730158bcccf4f8d9fc48e5699fb8c0c14347
marcinjak9 <marcinjak9@gmail.com>
2021-12-09 17:21:50 +0100

    added counter.dev

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <script dangerouslySetInnerHTML={{ __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"askatasuna",utcoffset:"1"}))};sessionStorage.setItem("_swa","1");`}}>
+        </script>
         </Head>
         <body>
           <Main />
